6bbc91b31e00bfc8191206ec4680b1b3b65a8b9b
Brian Mavity <brian@brianmavity.com>
2011-11-08 11:39:24 -0600

    Can create html files

--- bin/operatic.js
+++ bin/operatic.js
@@ -1,3 +1,29 @@
-exports.sayHi = function() {
-  console.log('hi');
+#!/usr/bin/env node
+
+var program = require('commander')
+  , fs = require('fs')
+  , path = require('path')
+  , util = require('util')
+  , templateFolder = path.join(__dirname, 'templates')
+  ;
+
+program
+  .option('-c, --create [output file]', 'create a file')
+  ;
+
+program.on('--create', function() {
+  var inputPath = path.join(templateFolder, 'template.html')
+    , outputPath = path.join(process.cwd(), program.create)
+    ;
+  if(path.existsSync(inputPath) && !path.existsSync(outputPath)) {
+    util.pump(fs.createReadStream(inputPath), fs.createWriteStream(outputPath), function(err) {
+      console.log(err);
+    })
+  }
+});
+
+program.parse(process.argv);
+
+if(program.create) {
+  program.emit('--create');
 }
